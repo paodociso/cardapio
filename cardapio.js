6e26da60c0e13e6eb29dfb7cbe2fff246760f1c5
paodociso <paodociso@gmail.com>
2026-01-05 18:33:12 -0300

    Add files via upload

--- cardapio.js
+++ cardapio.js
@@ -1,17 +1,25 @@
 // cardapio.js
 const dadosIniciais = {
   opcionais: [
-    // - Antipasti (potinhos de 100 gramas)
-    //["Azeitonas pretas (100g)", "15.00"],
-    //["Azeitonas verdes (100g)", "15.00"],
+    // - Antipasti - porções
     ["Alichella (100g)", "20.00"],
     ["Sardella (100g)", "20.00"],
-    ["Manteiga temperada (100g)", "17.00"],
     ["Melanzane Sott'olio (100g)", "20.00"],
-    ["Picles de cebola (100g)", "15.00"],
+    ["Manteiga temperada (100g)", "17.00"],
     ["Confit de alho (100g)", "20.00"],
     ["Confit de alho e tomates cereja (100g)", "20.00"],
+    ["Picles de cebola (100g)", "15.00"],   
     ["Pote de vidro", "5.00"],
+
+    // - Antipasti - panini
+    ["Alichella", "3.00"],
+    ["Sardella", "3.00"],
+    ["Melanzane sott'olio", "3.00"],
+    ["Manteiga", "2.00"],
+    ["Manteiga temperada com alho e parmesão", "3.00"],
+    ["Confit de alho", "2.00"],
+    ["Confit de alho e tomates cereja", "3.00"],
+    ["Picles de cebola", "2.00"],
 
     // - frios
     ["Bresaola", "9.00"],
@@ -35,7 +43,7 @@ const dadosIniciais = {
     ["Rúcula", "0.00"],
     ["Tomate", "0.00"],
 
-    // - complementos
+    // - temperos e pastinhas
     ["Azeite", "1.00"],
     ["Pesto tradicional", "2.00"],
     ["Pesto de azeitona", "2.00"],
@@ -44,153 +52,123 @@ const dadosIniciais = {
     ["Pastinha de goronzola", "2.00"],
     ["Pastinha de parmesão", "2.00"],
     ["Pastinha de pimentão vermelho", "2.00"],
-    ["Sardella", "3.00"],
-    ["Alichella", "3.00"],
-    ["Melanzane sott'olio", "3.00"],
-    ["Confit de alho", "2.00"],
-    ["Confit de alho e tomates cereja", "3.00"],
-    ["Manteiga", "2.00"],
-    ["Manteiga temperada com alho e parmesão", "3.00"]
   ],
 
   secoes: [
     {
       nome: "Antipasti",
       itens: [
-        [
-          "Sardella – 100g", 
-          "20.00", 
-          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjQr6FhXNpHgq1UNH0_J2bSno3jBTP98sUzlCXzFAAHXXdPFD2ml002SzVE-fvX8pnZ7TXAPZ45fcbMvN6oypcIspRmQzvSvyH9On2BUgnWzrEt77mQWdDo5XSk0hsrg5OrfCEGZWB1a-7htRxs10OhXCM504N4-b1mSmK8FX5z9O4d84Y3pri-cJtqUOE/s320/IMG_20201202_174720.jpg", 
-          ["Pote de vidro"], 
-          "Tradicional antepasto italiano à base de pimentão vermelho e aliche."
-        ],
-        [
-          "Alichella – 100g", 
-          "20.00", 
-          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjB6CH6FBqAhBXaVZiqLdjRqhlXGeDsrhkxKNgpRaeZw6lVWyGJ92hhVzNU4GxLFqq_B651AwJVwi1axSj58lPa6bkByQtR8RuSBk2EkdFLmPhcb-y3V4yyt-WOXjCBvVsaRbS2IsIW_b4YglTwG9YkHncLqWdrEo7SiFdLT3f7KIq4Mfi3ayKaN_vJSp4/s320/IMG_20201202_174740.jpg", 
-          ["Pote de vidro"], 
-          "Conserva de aliche (anchovas) com salsa e azeite extra virgem."
-        ],
-        [
-          "Melanzane Sott'olio – 100g", 
-          "20.00", 
-          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjygUAEJw4jjQR0fWMII5hc7QX1Ppu22I32LXjNTfcxf7B43coy-6gOXmdPiwCrL_NyWFuPM3HYzifiS_CuiqAD6efRxq9eYdbrr5I8C7x_jyn6I3w-OBplN5w184KYh5ZDnSF6iR6Bl6HQP5kRz8iycmQtRIfbeSR-5WvkfyQ7CFOuQKVLwwNPs5H_nj4/s4032/IMG_20201202_174658.jpg", 
-          ["Pote de vidro"], 
-          "Berinjela em conserva, marinada com alho e ervas em azeite de oliva, um clássico italiano."
-        ],
-        [
-          "Manteiga temperada – 100g", 
-          "17.00", 
-          "https://i0.wp.com/essareceitafunciona.com.br/wp-content/uploads/2022/08/Manteiga-temperada-Essa-Receita-Funciona-1.jpg?resize=683%2C1024&ssl=1", 
-          ["Pote de vidro"], 
-          "Manteiga amanteigada com ervas frescas e temperos, perfeita para untar pães."
-        ],
-        [
-          "Picles de cebola – 100g", 
-          "17.00", 
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj4_qCb1HDgQQJ_28yAzCCZ1DkiP9OQNHrQA&s", 
-          ["Pote de vidro"], 
-          "Cebolas em conserva agridoce, crocantes e levemente ácidas."
-        ],
-        [
-          "Confit de alho – 100g", 
-          "20.00", 
-          "https://francinha.com/novo/wp-content/uploads/2020/12/alho-confit-10.jpg", 
-          ["Pote de vidro"], 
-          "Alho cozido lentamente em azeite até ficar cremoso e adocicado."
-        ],
-        [
-          "Confit de alho e tomates cereja – 100g", 
-          "20.00", 
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg-1J_u_Kx2CIKSxLN1kASv_KQoogDYMioEA&s", 
-          ["Pote de vidro"], 
-          "Alho confitado com tomates cereja, uma combinação doce e intensa."
-        ]
+        ["Sardella (porção de 100g)", 
+         "20.00", 
+         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjQr6FhXNpHgq1UNH0_J2bSno3jBTP98sUzlCXzFAAHXXdPFD2ml002SzVE-fvX8pnZ7TXAPZ45fcbMvN6oypcIspRmQzvSvyH9On2BUgnWzrEt77mQWdDo5XSk0hsrg5OrfCEGZWB1a-7htRxs10OhXCM504N4-b1mSmK8FX5z9O4d84Y3pri-cJtqUOE/s320/IMG_20201202_174720.jpg", 
+         ["Pote de vidro"], 
+         "Tradicional antepasto italiano."],
+        
+        ["Alichella (porção de 100g)", 
+         "20.00", 
+         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjB6CH6FBqAhBXaVZiqLdjRqhlXGeDsrhkxKNgpRaeZw6lVWyGJ92hhVzNU4GxLFqq_B651AwJVwi1axSj58lPa6bkByQtR8RuSBk2EkdFLmPhcb-y3V4yyt-WOXjCBvVsaRbS2IsIW_b4YglTwG9YkHncLqWdrEo7SiFdLT3f7KIq4Mfi3ayKaN_vJSp4/s320/IMG_20201202_174740.jpg", 
+         ["Pote de vidro"], 
+         "Conserva de aliche com salsa."],
+        
+        ["Melanzane Sottolio (porção de 100g)", 
+         "20.00", 
+         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjygUAEJw4jjQR0fWMII5hc7QX1Ppu22I32LXjNTfcxf7B43coy-6gOXmdPiwCrL_NyWFuPM3HYzifiS_CuiqAD6efRxq9eYdbrr5I8C7x_jyn6I3w-OBplN5w184KYh5ZDnSF6iR6Bl6HQP5kRz8iycmQtRIfbeSR-5WvkfyQ7CFOuQKVLwwNPs5H_nj4/s4032/IMG_20201202_174658.jpg", 
+         ["Pote de vidro"], 
+         "Berinjela em conserva italiana."],
+        
+        ["Manteiga temperada (porção de 100g)", 
+         "17.00", 
+         "https://i0.wp.com/essareceitafunciona.com.br/wp-content/uploads/2022/08/Manteiga-temperada-Essa-Receita-Funciona-1.jpg?resize=683%2C1024&ssl=1", 
+         ["Pote de vidro"], 
+         "Manteiga com ervas frescas."],
+        
+        ["Picles de cebola (porção de 100g)", 
+         "17.00", 
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj4_qCb1HDgQQJ_28yAzCCZ1DkiP9OQNHrQA&s", 
+         ["Pote de vidro"], 
+         "Cebolas em conserva agridoce."],
+        
+        ["Confit de alho (porção de 100g)", 
+         "20.00", 
+         "https://francinha.com/novo/wp-content/uploads/2020/12/alho-confit-10.jpg", 
+         ["Pote de vidro"], 
+         "Alho cozido lentamente."],
+        
+        ["Confit de alho e tomates cereja (porção de 100g)", 
+         "20.00", 
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg-1J_u_Kx2CIKSxLN1kASv_KQoogDYMioEA&s", 
+         ["Pote de vidro"], 
+         "Alho com tomates cereja."]
       ]
     },
     {
       nome: "Pães de fermentação natural",
       itens: [
-        [
-          "Pão Italiano", 
-          "35.00", 
-          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMFsu1AGezB-IwKPTAOIKWgIFGjyNAdFfGctT8EsfFLIPX0dFTpVSVuCgF2MYYpmoc1n30aJubSS1I2u6WNmueDDioKeV6yVLIC46C_vxPLxuN82nA4P5kPfHF6A2Qq67v4STkPL4a_ZrYkQCy7OmrEP4qCAjj79bm0Jst4LpKEd0VgU9pzRhgPSmvZY8/s320/IMG_20201101_192911-01.jpeg", 
-          [
-            "Sardella (100g)", 
-            "Alichella (100g)", 
-            "Melanzane Sott'olio (100g)", 
-            "Manteiga temperada (100g)", 
-            "Picles de cebola (100g)",
-            "Confit de alho (100g)",
-            "Confit de alho e tomates cereja (100g)",
-            "Pote de vidro"
-          ], 
-          "Pão clássico de casca grossa e miolo aerado, fermentado por pelo menos 24 horas."
-        ],
-        [
-          "Pão de Campanha", 
-          "35.00", 
-          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMFsu1AGezB-IwKPTAOIKWgIFGjyNAdFfGctT8EsfFLIPX0dFTpVSVuCgF2MYYpmoc1n30aJubSS1I2u6WNmueDDioKeV6yVLIC46C_vxPLxuN82nA4P5kPfHF6A2Qq67v4STkPL4a_ZrYkQCy7OmrEP4qCAjj79bm0Jst4LpKEd0VgU9pzRhgPSmvZY8/s320/IMG_20201101_192911-01.jpeg", 
-          [
-            "Sardella (100g)", 
-            "Alichella (100g)", 
-            "Melanzane Sott'olio (100g)", 
-            "Manteiga temperada (100g)", 
-            "Picles de cebola (100g)",
-            "Confit de alho (100g)",
-            "Confit de alho e tomates cereja (100g)",
-            "Pote de vidro"
-          ], 
-          "Pão rústico de fermentação natural, tradicionalmente elaborado com farinha de trigo refinada com um toque de centeio ou integral, seu sabor e aroma são mais ricos do que de um pão italiano."
-        ]
+        ["Pão Italiano", 
+         "35.00", 
+         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMFsu1AGezB-IwKPTAOIKWgIFGjyNAdFfGctT8EsfFLIPX0dFTpVSVuCgF2MYYpmoc1n30aJubSS1I2u6WNmueDDioKeV6yVLIC46C_vxPLxuN82nA4P5kPfHF6A2Qq67v4STkPL4a_ZrYkQCy7OmrEP4qCAjj79bm0Jst4LpKEd0VgU9pzRhgPSmvZY8/s320/IMG_20201101_192911-01.jpeg", 
+         ["Sardella", 
+          "Alichella", 
+          "Melanzane Sott'olio", 
+          "Manteiga temperada", 
+          "Picles de cebola", 
+          "Confit de alho", 
+          "Confit de alho e tomates cereja", 
+          "Pote de vidro"], 
+         "Pão clássico aerado."],
+        
+        ["Pão de Campanha", 
+         "35.00", 
+         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMFsu1AGezB-IwKPTAOIKWgIFGjyNAdFfGctT8EsfFLIPX0dFTpVSVuCgF2MYYpmoc1n30aJubSS1I2u6WNmueDDioKeV6yVLIC46C_vxPLxuN82nA4P5kPfHF6A2Qq67v4STkPL4a_ZrYkQCy7OmrEP4qCAjj79bm0Jst4LpKEd0VgU9pzRhgPSmvZY8/s320/IMG_20201101_192911-01.jpeg", 
+         ["Sardella", 
+          "Alichella", 
+          "Melanzane Sott'olio", 
+          "Manteiga temperada", 
+          "Picles de cebola", 
+          "Confit de alho", 
+          "Confit de alho e tomates cereja", 
+          "Pote de vidro"], 
+         "Pão rústico com centeio."]
       ]
     },
     {
       nome: "Panini",
       itens: [
-        [
-          "Monte seu Panino", 
-          "5.00", 
-          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgg1s3_8QoPb0AIrSs8NwwEG4P1-RtRaHPN1m0nnt6SDrelqKBNmFelf24iT__zBjsbIwIIiYrgPyYAIvB-0Sxwm70qshjJ52bANJ5tz57yffbvGq2ml8M7TADZfvZeMN28FsOHkqfpg0CB3rIfJU5klY1VGDDzDOz-fzu-N3nRSsJJZQLZFER8ZuYglek/s320/Focaccia-de-mortadella-con-burrata-y-pesto.jpg", 
-          [
-            // Frios
-            "Bresaola", 
-            "Lombo piripiri", 
-            "Mortadela", 
-            "Pastrame", 
-            "Presunto cru", 
-            "Salame",
-            // Queijos
-            "Chancliche", 
-            "Chancliche apimentado", 
-            "Muçarela", 
-            "Provolone", 
-            "Queijo Prato",
-            // Vegetais
-            "Alface", 
-            "Pepino", 
-            "Pimentão", 
-            "Rúcula", 
-            "Tomate",
-            // Complementos
-            "Azeite", 
-            "Pesto tradicional", 
-            "Pesto de azeitona", 
-            "Maionese", 
-            "Maionese temperada", 
-            "Pastinha de goronzola", 
-            "Pastinha de parmesão", 
-            "Pastinha de pimentão vermelho", 
-            "Sardella", 
-            "Alichella", 
-            "Melanzane sott'olio",
-            "Confit de alho",
-            "Confit de alho e tomates cereja",
-            "Manteiga",
-            "Manteiga temperada com alho e parmesão"
-          ], 
-          "Base de pão artesanal. Escolha seus acompanhamentos abaixo."
-        ]
+        ["Monte seu Panino", 
+         "5.00", 
+         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgg1s3_8QoPb0AIrSs8NwwEG4P1-RtRaHPN1m0nnt6SDrelqKBNmFelf24iT__zBjsbIwIIiYrgPyYAIvB-0Sxwm70qshjJ52bANJ5tz57yffbvGq2ml8M7TADZfvZeMN28FsOHkqfpg0CB3rIfJU5klY1VGDDzDOz-fzu-N3nRSsJJZQLZFER8ZuYglek/s320/Focaccia-de-mortadella-con-burrata-y-pesto.jpg", 
+         ["Bresaola", 
+          "Lombo piripiri", 
+          "Mortadela", 
+          "Pastrame", 
+          "Presunto cru", 
+          "Salame", 
+          "Chancliche", 
+          "Chancliche apimentado", 
+          "Muçarela", 
+          "Provolone", 
+          "Queijo Prato", 
+          "Alface", 
+          "Pepino", 
+          "Pimentão", 
+          "Rúcula", 
+          "Tomate", 
+          "Azeite", 
+          "Pesto tradicional", 
+          "Pesto de azeitona", 
+          "Maionese", 
+          "Maionese temperada", 
+          "Pastinha de goronzola", 
+          "Pastinha de parmesão", 
+          "Pastinha de pimentão vermelho", 
+          "Sardella", 
+          "Alichella", 
+          "Melanzane sott'olio", 
+          "Confit de alho", 
+          "Confit de alho e tomates cereja", 
+          "Manteiga", 
+          "Manteiga temperada com alho e parmesão"], 
+         "Base de pão artesanal."]
       ]
     }
   ]
