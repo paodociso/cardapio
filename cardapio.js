6bb80a9af2b134b576f4d713654de443c8ff5749
paodociso <paodociso@gmail.com>
2026-01-05 09:30:21 -0300

    Add files via upload

---
+++ cardapio.js
@@ -0,0 +1,19 @@
+var dadosIniciais = {
+  "opcionais": [
+    ["Manteiga de Ervas", 8.50],
+    ["Geleia Real", 12.00],
+    ["Cream Cheese", 10.00]
+  ],
+  "secoes": [
+    {
+      "nome": "Pães Artesanais",
+      "itens": [
+        ["Sourdough Tradicional", 28.00, "https://via.placeholder.com/80", ["Manteiga de Ervas", "Geleia Real"]],
+        ["Sourdough Integral", 30.00, "https://via.placeholder.com/80", ["Manteiga de Ervas", "Geleia Real"]],
+        ["Pão de Campanha", 26.00, "https://via.placeholder.com/80", ["Manteiga de Ervas"]],
+        ["Baguette", 15.00, "https://via.placeholder.com/80", ["Cream Cheese"]],
+        ["GUARANA", 8.00, "https://via.placeholder.com/80", []]
+      ]
+    }
+  ]
+};
