953640d414ca83fe88e8b7939f06239b4ded94a0
paodociso <paodociso@gmail.com>
2026-01-05 10:02:01 -0300

    Add files via upload

--- cardapio.js
+++ cardapio.js
@@ -1,18 +1,21 @@
 var dadosIniciais = {
   "opcionais": [
-    ["Manteiga de Ervas", 8.50],
-    ["Geleia Real", 12.00],
-    ["Cream Cheese", 10.00]
+    ["Azeite", 2.00],
+    ["Queijo Prato", 7.00]
   ],
   "secoes": [
     {
       "nome": "Pães Artesanais",
       "itens": [
-        ["Sourdough Tradicional", 28.00, "https://via.placeholder.com/80", ["Manteiga de Ervas", "Geleia Real"]],
-        ["Sourdough Integral", 30.00, "https://via.placeholder.com/80", ["Manteiga de Ervas", "Geleia Real"]],
-        ["Pão de Campanha", 26.00, "https://via.placeholder.com/80", ["Manteiga de Ervas"]],
-        ["Baguette", 15.00, "https://via.placeholder.com/80", ["Cream Cheese"]],
-        ["GUARANA", 8.00, "https://via.placeholder.com/80", []]
+        ["Pão de Campanha", 35.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUY8TjMSdRmxRba1GVAJRjccC869kEO0EsOg&s", []],
+        ["Pão Italiano", 35.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5zViX6pGuk75NheWEWXZcWRqcbGIOX12i1g&s", []]
+      ]
+    },
+    {
+      "nome": "Panini",
+      "itens": [
+        ["Panino de Salame", 18.00, "https://www.sadia.com.br/assets/images/_/recipes/1774e4f774dad206b16c392e910196e9fa8c8b0f.webp", ["Azeite", "Queijo Prato"]],
+        ["Panino de Mortadela", 18.00, "https://www.seara.com.br/wp-content/uploads/2025/09/sanduiche-de-mortadela-com-burrata-na-focaccia-portal-minha-receita.webp", ["Azeite", "Queijo Prato"]]
       ]
     }
   ]
